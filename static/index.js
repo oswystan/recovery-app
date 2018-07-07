b633d8afa0d3b07ea4c7a4578718cd62d826029b
oswystan <oswystan@126.com>
2018-07-07 17:36:16 +0800

    feature: add server handlers

--- static/index.js
+++ static/index.js
@@ -1,3 +1,22 @@
 (function(){
-    console.log("hello");
+    let ws = new WebSocket("wss://localhost:8443/app/v1.0.0");
+    ws.onopen = function() {
+        console.log("open");
+        sendPing();
+    };
+    ws.onclose = function() {
+        console.log("close");
+    }
+
+    ws.onmessage = (msg)=>{
+        console.log(msg.data);
+    };
+
+    function sendPing() {
+        let req = {
+            command: "unpublish",
+            sid: 112,
+        };
+        ws.send(JSON.stringify(req));
+    }
 })();
